a159f0179ec0413d42a606a2df5058c0881f9281
Alen James <alenjames899@gmail.com>
2024-11-14 10:55:56 +0530

    Add root route and update CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@ const cors = require("cors"); // Import cors
 require('dotenv').config(); // Load environment variables from .env
 
 const app = express();
-
+app.get("/", (req, res) => {
+    res.send("Welcome to the API!"); // Basic message for the root route
+  });
+  
 // Enable CORS for requests from http://localhost:5173
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: ["http://localhost:5173", "https://heyyauto-ds4z.vercel.app/"] }));
+
 
 // Connect to MongoDB
 connectDB();
@@ -16,6 +20,7 @@ app.use(express.json()); // Middleware to parse JSON
 
 // Route mounting
 app.use("/", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
